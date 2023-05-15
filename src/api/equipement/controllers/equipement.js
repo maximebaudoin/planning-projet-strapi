c2c2cd238b2d1b6432781a86d2f421c8990645f8
Antonin SIMON <antonin.simon@epsi.fr>
2023-05-15 09:57:28 +0200

    add equipement collection

---
+++ src/api/equipement/controllers/equipement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * equipement controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::equipement.equipement');
---
+++ src/api/equipement/routes/equipement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * equipement router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::equipement.equipement');
